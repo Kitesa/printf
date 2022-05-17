ba51175e504be9d030b322017d43d8bd7491ccd4
kitesa Merga <67538957+Kitesa@users.noreply.github.com>
2022-05-17 11:06:20 -0400

    Update main.h
    Added main.h  header

--- main.h
+++ main.h
@@ -1,16 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
-#include <stdio.h>
 #include <stdarg.h>
+#include <stddef.h>
 #include <stdlib.h>
 
 /**
- * struct structprint - structure 
- * @q: location and method to change data to char
- * @u: print function for specific type
+ * struct structprint - structure containing
+ * @q: the location and method to translate data to characters.
+ * @u: print function for specific type.
  *
- * Return:integer
+ * Return: int
  */
 typedef struct structprint
 {
@@ -18,18 +18,17 @@ typedef struct structprint
 	int (*u)(char *format, va_list);
 } structype;
 
-int _printf(char *format, ...);
 int _putchar(char ch);
 int _puts(char *string);
 int printc(char *format, va_list);
 int printstr(char *format, va_list);
 int (*driver(char *format))(char *format, va_list);
 int _printf(char *format, ...);
-int printint(char *format, va_list ap);
+int printint(char *format, va_list pa);
 int integer(int number);
 int contadordigit(int number);
 int _abs(int number);
-int printpercent(char *format, va_list ap);
+int printpercent(char *format, va_list pa);
 int printhex(char *format, va_list);
 int printHEX(char *format, va_list);
 int printocta(char *format, va_list);
