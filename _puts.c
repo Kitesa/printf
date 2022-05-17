68897e4f32dc9ed8247e10147f5eaa510b9a7002
kitesa Merga <67538957+Kitesa@users.noreply.github.com>
2022-05-17 11:12:28 -0400

    Update _puts.c
    added putc

--- _puts.c
+++ _puts.c
@@ -2,13 +2,14 @@
 
 /**
  * _puts - prints a string
+ * new line, to stdout.
  * @string: input string
- * Return: count number of strings
+ * Return: count of string.
  */
-
 int _puts(char *string)
 {
 	int contador = 0;
+
 	while (*string)
 	{
 		_putchar(*string);
