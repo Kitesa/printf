6d2b6ed70fabd5d7d79599b0f19af8a6e40bde46
kitesa Merga <67538957+Kitesa@users.noreply.github.com>
2022-05-17 11:03:05 -0400

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -3,20 +3,19 @@
 /**
  * _printf - prints formatted data to stdout
  * @format: string that contains the format to print
- * Return: number of characters
+ * Return: number of characters written
  */
 int _printf(char *format, ...)
 {
 	int written = 0, (*structype)(char *, va_list);
 	char q[3];
-	va_list ap;
+	va_list pa;
 
 	if (format == NULL)
 		return (-1);
 	q[2] = '\0';
-	va_start(ap, format);
+	va_start(pa, format);
 	_putchar(-1);
-
 	while (format[0])
 	{
 		if (format[0] == '%')
@@ -26,7 +25,7 @@ int _printf(char *format, ...)
 			{
 				q[0] = '%';
 				q[1] = format[1];
-				written += structype(q, ap);
+				written += structype(q, pa);
 			}
 			else if (format[1] != '\0')
 			{
@@ -46,8 +45,6 @@ int _printf(char *format, ...)
 			format++;
 		}
 	}
-
 	_putchar(-2);
 	return (written);
 }
-
